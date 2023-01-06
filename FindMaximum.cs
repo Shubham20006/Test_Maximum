1e3a158e6f075cda796402cf76b43583a471f84a
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-06 15:45:07 +0530

    Refactor to the create one generic class and finding maximum

--- FindMaximum.cs
+++ FindMaximum.cs
@@ -8,7 +8,14 @@ namespace GenericsDemo
 {
     public class FindMaximum<T> where T:IComparable
     {
-        public static T MaxIntNum(T A, T B, T C)
+        public T A, B, C;
+        public FindMaximum(T A,T B, T C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+        public static T MaxNum(T A, T B, T C)
         {
             if(A.CompareTo(B) > 0 && A.CompareTo(C)>0 || 
                     A.CompareTo(B) >= 0 && A.CompareTo(C) > 0 || 
@@ -30,53 +37,17 @@ namespace GenericsDemo
             {
                 return C;
             }
-            return A;
-        }
-        public static T MaxFloatNum(T P, T Q, T R)
+            return default;
+        
+       
+            
+        
+    }
+        public T MaxMethod()
         {
-            if (P.CompareTo(Q) > 0 && P.CompareTo(R) > 0 ||
-                    P.CompareTo(Q) >= 0 && P.CompareTo(R) > 0 ||
-                    P.CompareTo(Q) > 0 && P.CompareTo(R) >= 0)
-            {
-                return P;
-            }
-            if (Q.CompareTo(P) > 0 && Q.CompareTo(R) > 0 ||
-                   Q.CompareTo(P) >= 0 && Q.CompareTo(R) > 0 ||
-                   Q.CompareTo(P) > 0 && Q.CompareTo(R) >= 0)
-            {
-                return Q;
-            }
-            if (R.CompareTo(Q) > 0 && R.CompareTo(P) > 0 ||
-                   R.CompareTo(Q) >= 0 && R.CompareTo(P) > 0 ||
-                   R.CompareTo(Q) > 0 && R.CompareTo(P) >= 0)
-            {
-                return R;
-            }
-            return P;
-
-        }
-        public static T MaxString(T X, T Y, T Z)
-        {
-            if (X.CompareTo(Y) > 0 && X.CompareTo(Z) > 0 ||
-                    X.CompareTo(Y) >= 0 && X.CompareTo(Z) > 0 ||
-                    X.CompareTo(Y) > 0 && X.CompareTo(Z) >= 0)
-            {
-                return X;
-            }
-
-            if (Y.CompareTo(X) > 0 && Y.CompareTo(Z) > 0 ||
-                    Y.CompareTo(X) >= 0 && Y.CompareTo(Z) > 0 ||
-                    Y.CompareTo(X) > 0 && Y.CompareTo(Z) >= 0)
-            {
-                return Y;
-            }
-            if (Z.CompareTo(Y) > 0 && Z.CompareTo(X) > 0 ||
-                    Z.CompareTo(Y) >= 0 && Z.CompareTo(X) > 0 ||
-                    Z.CompareTo(Y) > 0 && Z.CompareTo(X) >= 0)
-            {
-                return Z;
-            }
-            return X;
+            T Max = FindMaximum<T>.MaxNum(this.A, this.B, this.C);
+            return Max;
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,23 +5,13 @@
       
         public static void Main(string[] args)
         {
-            float A = 5.2F;
-            float B = 1.5F;
-            float C = 3.4F;
-            float val = FindMaximum<float>.MaxIntNum(A, B, C);
-            Console.WriteLine("The greater Float num is   : " + val);
+           
 
-            int P = 5;
-            int Q = 15;
-            int R = 14;
-            int NUM = FindMaximum<int>.MaxFloatNum(P, Q, R);
-            Console.WriteLine("The greater Integer num is : " + NUM);
+           
+           int D= FindMaximum<int>.MaxNum(5,15,24);
+            Console.WriteLine("The greater Integer num is : " + D);
 
-            string X = "Banana";
-            string Y = "Peach";
-            string Z = "Apple";
-            string U = FindMaximum<string>.MaxString(X,Y,Z);
-            Console.WriteLine("The maximum String is      : " + U);
+
         }
     }
 }
